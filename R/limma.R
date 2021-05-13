74bc1b97ba7b6c3f2774def8ca79bd696d3defb0
Jeason <2055469819@qq.com>
2021-05-13 15:33:25 +0800

    fixed the mistake code

--- R/limma.R
+++ R/limma.R
@@ -15,14 +15,9 @@
 #' @param groups expressions, or character strings which can be parsed to expressions,
 #' specifying contrasts, it corresponds to \code{\link[limma]{makeContrasts}}.
 #' @return A dataframe with a row for the number top genes and the following columns:
-#'   \item{genelist}{one or more columns of probe annotation, if genelist was included as input}
 #'   \item{logFC}{estimate of the log2-fold-change corresponding to the effect or contrast
-#'   (for \code{topTableF} there may be several columns of log-fold-changes)}
-#'   \item{CI.L}{left limit of confidence interval for \code{logFC} (if \code{confint=TRUE} or \code{confint} is numeric)}
-#'   \item{CI.R}{right limit of confidence interval for \code{logFC} (if \code{confint=TRUE} or \code{confint} is numeric)}
 #'   \item{AveExpr}{average log2-expression for the probe over all arrays and channels, same as Amean in the \code{MarrayLM} object}
 #'   \item{t}{moderated t-statistic (omitted for \code{topTableF})}
-#'   \item{F}{moderated F-statistic (omitted for \code{topTable} unless more than one coef is specified)}
 #'   \item{P.Value}{raw p-value}
 #'   \item{adj.P.Value}{adjusted p-value or q-value}
 #'   \item{B}{log-odds that the gene is differentially expressed (omitted for \code{topTreat})}
@@ -30,9 +25,13 @@
 #' @import limma
 #' @author Erjie Zhao <2055469819@qq.com>
 #'
+#' @examples
+#' data(eset)
+#' res <- diff_limma_array(array_mat = eset$array, label_list = eset$group, groups = c("brain-liver"))
+#'
 #' @export
 
-diff_limma_array <- function(count_mat, label_list, groups) {
+diff_limma_array <- function(array_mat, label_list, groups) {
 
   ## extract group
   group_list <- label_list[match(label_list[,1], colnames(array_mat)), 2]
@@ -49,8 +48,8 @@ diff_limma_array <- function(count_mat, label_list, groups) {
   fit <- lmFit(array_mat, design)
   fit2 <- contrasts.fit(fit, contrast_matrix)
   fit2 <- eBayes(fit2)
-  tempOutput <- topTable(fit2, coef = 1, number=Inf)
-  limma_deg <- na.omit(tempOutput)
+  tempoutput <- topTable(fit2, coef = 1, number = Inf)
+  limma_deg <- na.omit(tempoutput)
 
   return(limma_deg)
 }
@@ -70,14 +69,9 @@ diff_limma_array <- function(count_mat, label_list, groups) {
 #' and most robust approach to differential exis when the sequencing depth is reasonably
 #' consistent across the RNA samples.
 #' @return A dataframe with a row for the number top genes and the following columns:
-#'   \item{genelist}{one or more columns of probe annotation, if genelist was included as input}
 #'   \item{logFC}{estimate of the log2-fold-change corresponding to the effect or contrast
-#'   (for \code{topTableF} there may be several columns of log-fold-changes)}
-#'   \item{CI.L}{left limit of confidence interval for \code{logFC} (if \code{confint=TRUE} or \code{confint} is numeric)}
-#'   \item{CI.R}{right limit of confidence interval for \code{logFC} (if \code{confint=TRUE} or \code{confint} is numeric)}
 #'   \item{AveExpr}{average log2-expression for the probe over all arrays and channels, same as Amean in the \code{MarrayLM} object}
 #'   \item{t}{moderated t-statistic (omitted for \code{topTableF})}
-#'   \item{F}{moderated F-statistic (omitted for \code{topTable} unless more than one coef is specified)}
 #'   \item{P.Value}{raw p-value}
 #'   \item{adj.P.Value}{adjusted p-value or q-value}
 #'   \item{B}{log-odds that the gene is differentially expressed (omitted for \code{topTreat})}
@@ -86,6 +80,10 @@ diff_limma_array <- function(count_mat, label_list, groups) {
 #' @import edgeR
 #'
 #' @author Erjie Zhao <2055469819@qq.com>
+#'
+#' @examples
+#' data(OSCC)
+#' res <- diff_limma_count(count_mat = OSCC$count, label_list = OSCC$group, groups = c("normal-tumor"), methods = "voom")
 #'
 #' @export
 #'
@@ -114,25 +112,25 @@ diff_limma_count <- function(count_mat, label_list, groups, methods = c("voom", 
     fit <- lmFit(logCPM, design)
     fit <- eBayes(fit, trend = TRUE)
     tempoutput <- topTable(fit, coef  =ncol(design), number = Inf)
-  } else {
-    v <- voom(dgelist, design, plot = TRUE)
+  } else if (method == "voom") {
+    v <- voom(dgelist, design)
     fit <- lmFit(v, design)
     fit <- eBayes(fit)
     tempoutput <- topTable(fit, coef = ncol(design), number = Inf)
   }
 
   ## get results
-  limma_deg <- na.omit(tempOutput)
+  limma_deg <- na.omit(tempoutput)
 
   return(limma_deg)
 }
 
 
-#' @name diff_limma_fpkm
+#' @name diff_limma_normalize
 #' @title Do differential expression analysis on normalized RNA-seq data using limma
 #' @description Do differential expression analysis using \code{limma} package,
-#' this is for the normalized RNA-seq data, such as \code{TPM}, \code{RPKM}, etc.
-#' @usage diff_limma_fpkm(expr_mat, label_list, groups)
+#' this is for the normalized RNA-seq data, such as \code{FPKM}, \code{TPM}, \code{RPKM}, etc.
+#' @usage diff_limma_normalize(expr_mat, label_list, groups)
 #' @param expr_mat A matrix-like data object containing log-ratios or
 #' log-expression values for normalized RNA-seq data, with rows corresponding
 #' to genes and columns to samples.
@@ -142,14 +140,9 @@ diff_limma_count <- function(count_mat, label_list, groups, methods = c("voom", 
 #' specifying contrasts, it corresponds to \code{\link[limma]{makeContrasts}}.
 #'
 #' @return A dataframe with a row for the number top genes and the following columns:
-#'   \item{genelist}{one or more columns of probe annotation, if genelist was included as input}
 #'   \item{logFC}{estimate of the log2-fold-change corresponding to the effect or contrast
-#'   (for \code{topTableF} there may be several columns of log-fold-changes)}
-#'   \item{CI.L}{left limit of confidence interval for \code{logFC} (if \code{confint=TRUE} or \code{confint} is numeric)}
-#'   \item{CI.R}{right limit of confidence interval for \code{logFC} (if \code{confint=TRUE} or \code{confint} is numeric)}
 #'   \item{AveExpr}{average log2-expression for the probe over all arrays and channels, same as Amean in the \code{MarrayLM} object}
 #'   \item{t}{moderated t-statistic (omitted for \code{topTableF})}
-#'   \item{F}{moderated F-statistic (omitted for \code{topTable} unless more than one coef is specified)}
 #'   \item{P.Value}{raw p-value}
 #'   \item{adj.P.Value}{adjusted p-value or q-value}
 #'   \item{B}{log-odds that the gene is differentially expressed (omitted for \code{topTreat})}
@@ -157,10 +150,14 @@ diff_limma_count <- function(count_mat, label_list, groups, methods = c("voom", 
 #' @import limma
 #' @author Erjie Zhao <2055469819@qq.com>
 #'
+#' @examples
+#' data(OSCC)
+#' res <- diff_limma_normalize(expr_mat = OSCC$rpkm, label_list = OSCC$group, groups = c("normal-tumor"))
+#'
 #' @export
 #'
-#' reference：https://support.bioconductor.org/p/56275/#56299
-diff_limma_fpkm <- function(expr_mat, label_list, groups) {
+#' the reference：https://support.bioconductor.org/p/56275/#56299
+diff_limma_normalize <- function(expr_mat, label_list, groups) {
   ## check log2
   logs <- check_log2(expr_mat)
 
@@ -186,8 +183,8 @@ diff_limma_fpkm <- function(expr_mat, label_list, groups) {
   fit <- lmFit(expr_mat, design)
   fit2 <- contrasts.fit(fit, contrast_matrix)
   fit2 <- eBayes(fit2, robust = TRUE, trend = TRUE)
-  tempOutput <- topTable(fit2, coef = 1, number=Inf)
-  limma_deg <- na.omit(tempOutput)
+  tempoutput <- topTable(fit2, coef = 1, number=Inf)
+  limma_deg <- na.omit(tempoutput)
 
   return(limma_deg)
 }
