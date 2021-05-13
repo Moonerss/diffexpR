35e9ee7b980fcc43043462f71d3da67722fe9b20
Jeason <2055469819@qq.com>
2021-05-13 15:33:59 +0800

    add example data

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' microarray expression data
+#'
+#' A list contain a microarray probe profile and a sample groups
+#'
+#' @format
+#' \describe{
+#'   \item{array}{array probe profile}
+#'   \item{group}{sample group}
+#' }
+#'
+"eset"
+
+#' RNA-seq expression data
+#'
+#' A list contain raw read count data, normalize rpkm data, and sample groups
+#' @format
+#' \describe{
+#'   \item{count}{read count matrix}
+#'   \item{group}{sample group}
+#'   \item{rpkm}{normalize rpkm matrix}
+#' }
+#'
+"OSCC"
+
