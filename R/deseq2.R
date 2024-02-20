b0afc722f337eda51143a79ccb03e69ec54ddaf2
jeason <2055469819@qq.com>
2024-02-20 13:32:24 +0800

    change the usage of `diff_deseq2_count` function, add more print info

--- R/deseq2.R
+++ R/deseq2.R
@@ -8,12 +8,12 @@
 #' @title Do differential expression analysis on read count data using \code{DESeq2}
 #' @description Do differential expression analysis using \code{DESeq2} package,
 #' this is for the read count data in RNA-seq.
-#' @usage diff_deseq2_count(count_mat, label_list, groups)
 #' @param count_mat A matrix-like data object containing the read count data, with rows corresponding to genes and columns to samples.
 #' @param label_list a dataframe contain samples in first column and groups information
 #' in second column in the form of factors.
-#' @param groups the same as \code{contrast} argument in \code{\link[DESeq2]{results}}, as the name of a factor in the design formula is \code{condition}
+#' @param ref.groups a character string specifying the reference group. If specified, for a given grouping variable, each of the group levels will be compared to the reference group (i.e. control group).
 #' @return A dataframe with a row for the number top genes and the following columns:
+#' \item{Gene}{the name of genes}
 #' \item{baseMean}{}
 #' \item{log2FoldChange}{log2-fold change of expression between conditions being tested.}
 #' \item{lfcSE}{the standard error of the \code{log2FoldChange}}
@@ -21,27 +21,48 @@
 #' \item{pvalue}{p-values.}
 #' \item{padj}{adjusted p-values by 'BH'}
 #' @importFrom DESeq2 DESeqDataSetFromMatrix DESeq results
+#' @importFrom cli cli_alert_warning cli_alert_info cli_abort
 #' @author Erjie Zhao <2055469819@qq.com>
 #' @examples
 #' data(OSCC)
 #' res <- diff_deseq2_count(count_mat = OSCC$count, label_list = OSCC$group,
-#'                          groups = c("condition", "tumor", "normal"))
+#'                          ref.groups = "normal")
 #' @export
-diff_deseq2_count <- function(count_mat, label_list, groups) {
+diff_deseq2_count <- function(count_mat, label_list, ref.groups = NULL) {
   ## ref: https://www.plob.org/article/11506.html
-
   ## extract group
   condition <- label_list[match(label_list[,1], colnames(count_mat)), 2]
+
+  if (is.character(condition)) {
+    cli::cli_alert_warning('Set group information into factors')
+    condition <- as.factor(condition)
+  }
+
+  condition_level <- levels(condition)
+  cli::cli_alert_info('factor level: {.val {paste(condition_level, collapse = \' \')}}')
+
+
+  if (!is.element(ref.groups, condition_level)) {
+    cli::cli_abort(c('x' = '{.var ref.groups} must in {.var {condition_level}}'))
+  }
 
   ## construct DESeqDataSet object
   dds_obj <- DESeqDataSetFromMatrix(countData = count_mat,
                                     colData = data.frame(condition),
                                     design = ~ condition)
   ## differential expression analysis
-  dds <- DESeq(dds_obj)
-  res <- results(dds, contrast = groups)
+  vs_group <- setdiff(levels(condition), ref.groups)
+  contrast_group <- c('condition', vs_group, ref.groups)
+  dds <- DESeq(dds_obj, quiet = TRUE)
+  res <- results(dds, contrast = contrast_group)
+
 
   ## get result
-  res <- as.data.frame(res)
+  for (i in res@elementMetadata$description) {
+    cli::cli_alert_info(i)
+  }
+  res <- res %>% as.data.frame() %>%
+    tibble::rownames_to_column(var = 'Gene')
+
   return(res)
 }
